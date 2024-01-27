5a1b3372dfb90f69cd7ba7467562f5f07386243c
Mateus Barbosa <pg9779628@gmail.com>
2024-01-27 15:45:13 -0300

    Changing While to For!

--- MegaSena.java
+++ MegaSena.java
@@ -4,11 +4,9 @@ public class MegaSena {
     public static void main(String[] args) {
         Random generate = new Random();
 
-        int i = 0;
-        while (i < 6) {
+        for (int i = 0;i < 6;i++) {
             int number = generate.nextInt(60);
             System.out.println(number);
-            i++;
         }
     }
 }
